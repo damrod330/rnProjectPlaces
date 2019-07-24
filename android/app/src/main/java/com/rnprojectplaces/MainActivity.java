4fb312fc31cff025d8e48218114a0902329579c8
Konrad Baczyński <konrad.baczynski@almar.pl>
2019-07-24 09:26:40 +0200

    added react-native-navigation@1 library

--- android/app/src/main/java/com/rnprojectplaces/MainActivity.java
+++ android/app/src/main/java/com/rnprojectplaces/MainActivity.java
@@ -1,15 +1,8 @@
 package com.rnprojectplaces;
 
 import com.facebook.react.ReactActivity;
-
-public class MainActivity extends ReactActivity {
-
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    @Override
-    protected String getMainComponentName() {
-        return "rnProjectPlaces";
-    }
+import com.reactnativenavigation.controllers.SplashActivity;
+ 
+public class MainActivity extends SplashActivity {
+ 
 }
